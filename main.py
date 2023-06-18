bc1a4a57e4a54ef2a6cc26cf7b86f8700d3134cd
misha <mishaghezha@gmail.com>
2023-06-18 13:39:06 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+import time
+from decimal import Decimal
+
+import pymysql
+
+con = pymysql.connect(host='192.168.31.229', user='BigCat',
+                      password='0000', database='exchange_service')
+
+def getStock():
+    cur = con.cursor()
+    array = []
+    for id in range(1, 10):
+        cur.execute(f"SELECT price, amount FROM exchange_service.order_books where id = {id} and stock_id = {1}")
+        fetch = cur.fetchall()[0]
+        i = 0
+        while i < fetch[1]:
+            array.append(str(fetch[0]))
+            i += 1
+    print(array)
+    return array
+
+with con:
+    getStock()
+
+# multiplier = Decimal(random.uniform(0.95, 1.05))
+# new_price = fetch * multiplier
+# cur.execute(f"UPDATE order_books SET price = {new_price} WHERE id = {id}")
+# cur.execute(f"SELECT price FROM exchange_service.order_books where id = {id}")
+# fetch = cur.fetchall()[0][0]
